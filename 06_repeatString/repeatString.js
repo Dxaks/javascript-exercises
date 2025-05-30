9b2396c4abf053eb4166b04cd07cb8a22872e959
Zakariya Auwal <192784011+Dxaks@users.noreply.github.com>
2025-05-31 00:02:36 +0100

    modified:   repeatString.js

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+// const Guest = Math.floor(Math.random() * 500);
 
+
+const repeatString = function(message, times) {
+ let greet = ''
+ 
+ for (let i = 0; i < times; i++) {
+    greet += message;
+ }
+ return greet
 };
+
+console.log(repeatString('Hello', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
