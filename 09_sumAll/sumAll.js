d0568a9e8ea876e0b7e50b67a1b7f7536c36e074
Zakariya Auwal <192784011+Dxaks@users.noreply.github.com>
2025-06-03 19:32:56 +0100

    Implement getLeapYears function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if(!Number.isInteger(min) || !Number.isInteger(max)) return "Error";
 
+    if(min < 0 || max < 0) return "Error";
+
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+console.log(sumAll(1, 2));
 
 // Do not edit below this line
 module.exports = sumAll;
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,36 @@
-const leapYears = function() {
+const getleapYears = function() {
+
+    const startYear = function() {
+        let start = prompt('enter the starting year')
+        return parseInt(start.trim());
+}
+
+    const endYear = function() {
+        let end = prompt('enter the ending year')
+        return parseInt(end.trim());
+}
+
+    let start = startYear();
+    let end = endYear();
+
+    if (isNaN(start) || isNaN(end)) {
+        console.log('Please enter valid numbers for the years.');
+        return;
+    }
+
+    console.log(`Leap years between ${start} and ${end}:`);
+
+for (let i = start; i <= end; i++) {
+
+  if((i % 4 === 0 && i % 100 !== 0) || (i % 400 === 0))
+    console.log(i)
+};
 
 };
+
+getleapYears();
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
