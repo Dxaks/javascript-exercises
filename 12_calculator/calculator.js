f4f70e6d758de44fb719f193572a2f0e0e8a1cd5
Zakariya Auwal <192784011+Dxaks@users.noreply.github.com>
2025-07-06 13:08:54 +0100

    update my the Odin J.S Excercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,27 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let add = num1 + num2;
+  return add;
 };
 
-const subtract = function() {
-	
+
+const subtract = function(num1, num2) {
+  let sub = num1 - num2;
+  return sub;
 };
 
-const sum = function() {
-	
+
+const sum = function(arr) {
+  return arr.reduce((a, b) => {
+   return a + b;
+  },0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n == 0) n = n + 1;
+  let arr = [];
+  for (let i = n; i >= 1; i--) {
+    arr.push(i);
+  }
+  return arr.reduce((a, b) => a * b)
 };
+console.log(factorial(20))
 
+ 
 // Do not edit below this line
 module.exports = {
   add,
